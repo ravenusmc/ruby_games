823e3477c2a872d11be1a538160708255c71771c
ravenusmc <mcuddy77@gmail.com>
2016-05-08 17:25:06 -0400

    added first method to my cat and mouse game

---
+++ chase.rb
@@ -0,0 +1,11 @@
+#This is what will be what will be described as a cat and mouse game. 
+
+def main
+  puts "------------------------"
+  puts "      CAT AND MOUSE     "
+  puts "      STRATEGY GAME     "
+  puts "------------------------"
+  puts
+end 
+
+main
--- ideas.rb
+++ ideas.rb
@@ -11,26 +11,41 @@ end
 puts "Please select a position"
 choice = gets.chomp.to_i
 if choice == 1
-  board[0] = "y"
+  board[0] = "x"
+elsif choice == 2
+  board[1] = "x"
+elsif choice == 3
+  board[2] = "x"
+elsif choice == 4
+  board[3] = "x"
 end 
 
 display_board(board)
 
-puts "Please select another position"
-choice = gets.chomp.to_i
-if choice == 1
+# puts "Please select another position"
+# choice = gets.chomp.to_i
+# if choice == 1
   #delete_at method can be used to delete at a specific spot.
-  board.delete_at(0)
-  board[1] = "n"
+#   board.delete_at(0)
+#   board[1] = "o"
+# end 
+
+# display_board(board)
+
+puts "Please select a position"
+choice = gets.chomp.to_i
+if choice == 1 && board[0] == "x"
+  board[0] = "o"
 end 
 
 display_board(board)
 
 puts "Please select a position"
 choice = gets.chomp.to_i
-if choice == 1
-  board.delete_at(0)
-  board[0] = "y"
+if choice == 1 && board[0] == "o"
+  board[0] = "o"
+else 
+  board[0] = "x"
 end 
 
 display_board(board)
